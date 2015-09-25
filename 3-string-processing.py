d501a96a84972302eff100c07d46f73c057ae097
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2015-09-25 12:20:42 +0200

    Add second  example of string processing for third weeks lecture

--- 3-string-processing.py
+++ 3-string-processing.py
@@ -27,3 +27,30 @@ s5 = str(375)
 print s5[1:]
 i1 = int(s5[1:])
 print i1 + 38
+
+
+
+
+
+#############################################/
+#   second example of string processing
+#   money converter
+#############################################/
+
+# convert xx.yy to xx dollars and yy cents
+def convert(val):
+    dollars = int(val)
+    cents = int(100 * (val - dollars))
+    return str(dollars) + " dollars and " + str(cents) + " cents"
+
+# Tests
+print convert(11.23)
+print convert(11.20)
+print convert(1.12)
+print convert(12.01)
+print convert(1.01)
+print convert(0.01)
+print convert(1.00)
+print convert(0)
+print convert(-1.40)
+print convert(12.55555)
