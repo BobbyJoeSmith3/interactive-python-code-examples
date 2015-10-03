168ae016c3eb0d535a43c52ee0782dd282635c4f
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2015-10-03 09:39:33 -0500

    Add second example of keyboard input

--- 4-keyboard-input.py
+++ 4-keyboard-input.py
@@ -1,3 +1,7 @@
+#############################################/
+#   first example of keyboard input
+#############################################/
+
 # Keyboard echo
 
 import simplegui
@@ -33,3 +37,47 @@ f.set_draw_handler(draw)
 
 # start frame
 f.start()
+
+
+
+
+
+#############################################/
+#   second example of keyboard input
+#############################################/
+
+# control the position of a ball using the arrow keys
+
+import simplegui
+
+# Initialize globals
+WIDTH = 600
+HEIGHT = 400
+BALL_RADIUS = 20
+
+ball_pos = [WIDTH / 2, HEIGHT / 2]
+
+# define event handlers
+def draw(canvas):
+    canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
+
+def keydown(key):
+    vel = 4
+    if key == simplegui.KEY_MAP["left"]:
+        ball_pos[0] -= vel
+    elif key == simplegui.KEY_MAP["right"]:
+        ball_pos[0] += vel
+    elif key == simplegui.KEY_MAP["down"]:
+        ball_pos[1] += vel
+    elif key == simplegui.KEY_MAP["up"]:
+        ball_pos[1] -= vel
+
+# create frame
+frame = simplegui.create_frame("Positional ball control", WIDTH, HEIGHT)
+
+# register event handlers
+frame.set_draw_handler(draw)
+frame.set_keydown_handler(keydown)
+
+# start frame
+frame.start()
